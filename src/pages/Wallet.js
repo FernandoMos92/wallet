6fd744cce820a58fe7b3b1f8e713fdb910e519df
FernandoMos92 <fernandomosp@gmail.com>
2022-01-12 14:12:18 -0300

    feat: Add toastfy

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure();
 
 class Wallet extends Component {
+  componentDidMount() {
+    const notify = () => {
+      toast('Hello');
+    };
+    return notify();
+  }
+
   render() {
-    return <>ENTROU</>;
+    return <p> teste </p>;
   }
 }
 
