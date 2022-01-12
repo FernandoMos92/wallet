3a60494e701ce46056fa1f68134ce67aea25037f
FernandoMos92 <fernandomosp@gmail.com>
2022-01-11 22:31:03 -0300

    feat: Login page styled

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import Main from '../style/styledComponents';
+import Money from '../image/undraw_wallet_aym5.svg';
+import '../style/login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <Main>
+        <form>
+          <label htmlFor="inputEmail">
+            <input
+              type="text"
+              id="inputEmail"
+              data-testid="email-input"
+              placeholder="Email"
+            />
+          </label>
+          <label htmlFor="inputPassword">
+            <input
+              placeholder="Password"
+              type="password"
+              id="inputPassword"
+              data-testid="password-input"
+            />
+          </label>
+          <div className="container-remember">
+            <label htmlFor="input-remember">
+              Lembre de mim?
+              <input
+                type="checkbox"
+                className="input-remember"
+                id="input-remember"
+              />
+            </label>
+          </div>
+          <button type="submit"> Entrar </button>
+        </form>
+        <img src={ Money } alt="You wallet login" />
+      </Main>
+    );
   }
 }
 
---
+++ src/style/styledComponents.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+const Main = styled.div`
+display: flex;
+flex-flow: row nowrap;
+max-height: 100vh;
+max-width: 100vw;
+align-content: center;
+justify-content: center;
+margin-top: 15%;
+form{ 
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  padding: 30px;
+
+  input{ 
+    align-self: flex-end;
+    border: none;
+    border-bottom: 3px groove rgb(141,208,112);
+    border-radius: 2px;
+    margin-left: 8px;
+    width: 240px;
+    margin-bottom: 15px;
+    outline: none;
+  }
+
+  button{ 
+    margin-top: 20px;
+    width: 150px;
+    align-self: center;
+    background-color: rgb(80,79,96);
+    color: rgb(141,208,112);
+    border-radius: 4px;
+  }
+}
+`;
+
+export default Main;
