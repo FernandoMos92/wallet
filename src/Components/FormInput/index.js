75db6498a48b09d0d183ee88834fa211b7698840
FernandoMos92 <fernandomosp@gmail.com>
2022-01-13 12:26:21 -0300

    feat: create handleData

--- src/Components/FormInput/index.js
+++ src/Components/FormInput/index.js
@@ -7,43 +7,61 @@ class FormInput extends Component {
     super();
     this.state = {
       id: 0,
-      spent: 0,
+      value: '',
       description: '',
-      currencyCoin: '',
-      methodPag: '',
+      currency: '',
+      method: '',
       tag: '',
     };
 
-    this.saveInfo = this.saveInfo.bind(this);
+    this.handleData = this.handleData.bind(this);
   }
 
-  saveInfo({ target: { name, value } }) {
+  handleData({ target: { name, value } }) {
     this.setState({ [name]: value });
   }
 
   render() {
-    const { saveInfo } = this;
+    const {
+      state: {
+        id,
+        value,
+        description,
+        currency,
+        method,
+        tag,
+      }, handleData } = this;
     return (
       <FormCur>
         <label htmlFor="value-input">
           Valor:
           <input
+            name="value"
+            value={ value }
             data-testid="value-input"
             type="number"
+            onChange={ handleData }
           />
         </label>
+
         <label htmlFor="description-input">
           Descrição despesas:
           <input
+            name="description"
+            value={ description }
             data-testid="description-input"
             type="text"
+            onChange={ handleData }
           />
         </label>
         <label htmlFor="currency-input">
           Moeda:
           <select
+            name="currency"
+            value={ currency }
             data-testid="currency-input"
             type="select"
+            onChange={ handleData }
           >
             <option value="USD">USD</option>
             <option value="BTC">BTC</option>
@@ -53,30 +71,35 @@ class FormInput extends Component {
         <label htmlFor="method-input">
           Método de pagamento:
           <select
+            name="method"
+            value={ method }
             data-testid="method-input"
             type="select"
+            onChange={ handleData }
           >
-            <option value="Din">Dinheiro</option>
-            <option value="C/C">Cartão de crédito</option>
-            <option value="C/D">Cartão de débito</option>
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
           </select>
         </label>
         <label htmlFor="tag-input">
           Tag:
           <select
+            name="tag"
+            value={ tag }
             data-testid="tag-input"
             type="select"
+            onChange={ handleData }
           >
-            <option value="Alime">Alimentação</option>
+            <option value="Alimentação">Alimentação</option>
             <option value="Lazer">Lazer</option>
-            <option value="trab">Trabalho</option>
-            <option value="Trans">Transporte</option>
-            <option value="Saude">Saúde</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
           </select>
         </label>
         <button
           type="button"
-          onClick={ () => saveInfo() }
         >
           Adicionar despesa
         </button>
