a26147c6459a7fce47eae94685d4f1f83b27a960
FernandoMos92 <fernandomosp@gmail.com>
2022-01-13 23:00:05 -0300

    feat: Create table

--- src/Components/TableSpent/index.js
+++ src/Components/TableSpent/index.js
@@ -1,38 +1,23 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-
-const TSpent = styled.table`
-  
-`;
-
-const Tr = styled.tr`
-  display: flex;
-  flex-grow: 1;
-  min-width: 99vw;
-`;
-
-const Th = styled.th`
-  width: 100%;
-  border: 1px groove darkgray;
-  background-color: darkgray;
-  color: white;
-  padding: 10px;
-`;
 
 class TableSpent extends Component {
   render() {
     return (
-      <TSpent>
-        <Tr>
-          <Th>Valor</Th>
-          <Th>Descrição</Th>
-          <Th>Moeda</Th>
-          <Th>Método de pagamento</Th>
-          <Th>Tag</Th>
-          <Th>Câmbio utilizado</Th>
-          <Th>Valor convertido</Th>
-        </Tr>
-      </TSpent>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+      </table>
     );
   }
 }
